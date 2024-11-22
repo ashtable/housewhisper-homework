ac7561ab38e4cf6e07406f535dd2ee583a210ada
Ash Srinivas <ash@supallama.ai>
2024-11-22 07:35:57 -0800

    Added endpoint to check calendar for a meeting time

--- api.py
+++ api.py
@@ -9,8 +9,13 @@ import ics
 import pytz
 
 # Configure Logging
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
+
+
+class AgentId(str, Enum):
+    johndoe = "johndoe" 
+    janedoe = "janedoe"
 
 
 def pacific_time(utc_time: datetime) -> datetime:
@@ -22,6 +27,10 @@ def convert_to_serialiable_json(json: dict) -> dict:
     return (
         {key: value.isoformat() if isinstance(value, datetime) else value for key, value in json.items()}
     )
+
+
+def get_file_path(agent_id: AgentId) -> str:
+   return f"{agent_id.value}.ics" 
 
 
 def parse_ics_file(ics_file_path: str) -> list:
@@ -52,18 +61,12 @@ def parse_ics_file(ics_file_path: str) -> list:
     return events
 
 
-
-class AgentId(str, Enum):
-    johndoe = "johndoe" 
-    janedoe = "janedoe"
-
-
 app = FastAPI()
 
 
 @app.get("/download-calendar/{agent_id}")
 def download_calendar(agent_id: AgentId):
-    ics_file_path = f"{agent_id.value}.ics"
+    ics_file_path = get_file_path(agent_id)
     logger.info(f"Path: {ics_file_path}")
 
     if os.path.exists(ics_file_path):
@@ -74,7 +77,7 @@ def download_calendar(agent_id: AgentId):
 
 @app.get("/examine-calendar/{agent_id}")
 def examine_calendar(agent_id: AgentId):
-    ics_file_path = f"{agent_id.value}.ics"
+    ics_file_path = get_file_path(agent_id)
     logger.info(f"Path: {ics_file_path}")
 
     sorted_calendar_events = parse_ics_file(ics_file_path)
@@ -83,3 +86,40 @@ def examine_calendar(agent_id: AgentId):
     return JSONResponse(sorted_calendar_events)
 
 
+@app.get("/check-calendar/{agent_id}/{meeting_time_utc_start}/{meeting_time_utc_end}")
+def check_calendar(agent_id: AgentId, meeting_time_utc_start: datetime, meeting_time_utc_end: datetime) -> bool:
+    """Checks if an agent is available for 
+    a 1-hour meeting at the specified time"""
+
+    ics_file_path = get_file_path(agent_id)
+    logger.info(f"Path: {ics_file_path}")
+    logger.info(f"Requested Start Time: {pacific_time(meeting_time_utc_start)}")
+    logger.info(f"Requested End Time: {pacific_time(meeting_time_utc_end)}")
+
+
+    requested_start = meeting_time_utc_start
+    requested_end = meeting_time_utc_end
+
+    sorted_calendar_events = parse_ics_file(ics_file_path) 
+
+    for i in range(len(sorted_calendar_events)):
+        event = sorted_calendar_events[i]
+        event_start = datetime.fromisoformat(event['Start Time'])
+        event_end = datetime.fromisoformat(event['End Time'])
+        logger.debug(f"Event Start: {event_start}")
+        logger.debug(f"Event End: {event_end}")
+
+        if requested_end <= event_start:
+            # Since we're iterating through a sorted list of events, if we 
+            # come across an event that starts after the requested end time, 
+            # we know we can fit it into the calendar and return True.
+            return True
+        elif requested_start < event_end and event_start < requested_end:
+            # Similarly, if we come across an event in the calendar such 
+            # that the event ends after the requested start time AND
+            # the event starts before requested end time, we know that
+            # we cannot fit it into the calendar and return False.
+            return False
+
+
+    return True
