18d19843184099351201d2943e30f31c910cc9fc
Ash Srinivas <ash@supallama.ai>
2024-12-02 12:57:28 -0800

    Added underutilized endpoint

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
-from datetime import datetime, timedelta
+from datetime import date, datetime, time, timedelta
 from fastapi import FastAPI, HTTPException, Query
 from typing import Annotated
 
+from .constants import BUSINESS_DAY_START, BUSINESS_DAY_END, DEFAULT_TIMEZONE
 from .utils import preprocess_ics_files
 
 
@@ -206,3 +207,68 @@ async def coordinate(duration: int, time_range_start_datetime_in_default_tz: dat
     except Exception as e:
         return HTTPException(status_code=404, detail=str(e))
 
+
+@app.get("/underutilized/{agent_id}/{date_to_check}")
+async def underutilized(agent_id: int, date_to_check: date):
+    try:
+        # Preprocess ICS files for query
+        datastore = preprocess_ics_files(CONFIG)
+
+        # List to store our return value
+        available_times = []
+
+        # Set the duration we'll check for to 60 so that we
+        # only identify blocks of 1-hour or more as underutilized
+        duration = 60
+
+        # Create a pointer to iterate through the datastore
+        # starting at the beginning of date_to_check
+        time_pointer = datetime.combine(date_to_check, time(BUSINESS_DAY_START, 0), tzinfo=DEFAULT_TIMEZONE)
+        time_pointer_end = datetime.combine(date_to_check, time(BUSINESS_DAY_END, 0), tzinfo=DEFAULT_TIMEZONE)
+
+        # Set time_pointer to the start of the following quarter-hour
+        # so that we can suggest times at quarter-hour intervales
+        if time_pointer.minute > 0:
+            if time_pointer.minute < 15:
+                time_pointer = time_pointer.replace(minute=15)
+            elif time_pointer.minute < 30:
+                time_pointer = time_pointer.replace(minute=30)
+            elif time_pointer.minute < 45:
+                time_pointer = time_pointer.replace(minute=45)
+            else:
+                time_pointer += timedelta(hours=1)
+                time_pointer = time_pointer.replace(minute=0)
+
+        # Iterate from time_range_start to time_range_end
+        while time_pointer <= time_pointer_end:
+            print(f"Checking {time_pointer}")
+
+            # Check for the agent's availability Dict
+            if agent_id not in datastore:
+                raise LookupError("Unable to find requested agent_id")
+            
+            # Check for the Date's availability Dict
+            if time_pointer.date() not in datastore[agent_id]:
+                raise LookupError("Unable to find requested nested Date Dict")
+
+            # Check for the Hour's availability Dict
+            if time_pointer.hour not in datastore[agent_id][time_pointer.date()]:
+                raise LookupError("Unable to find requested nested Hour Dict")
+                
+            # Check for the Minutes's availability duratiion
+            if time_pointer.minute not in datastore[agent_id][time_pointer.date()][time_pointer.hour]:
+                raise LookupError("Unable to find requested nested Minute Duration")
+
+            min_available = datastore[agent_id][time_pointer.date()][time_pointer.hour][time_pointer.minute]
+            is_available = min_available >= duration
+
+            if is_available:
+                available_times.append(time_pointer)
+ 
+            # Increment by quarter-hour increments
+            time_pointer += timedelta(minutes=15)
+
+
+        return {"available_times": available_times}
+    except Exception as e:
+        return HTTPException(status_code=404, detail=str(e))
